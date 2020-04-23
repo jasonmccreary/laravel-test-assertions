fe21c6b4175f132c50aac26670fead0dc739f2ee
Jason McCreary <jason@pureconcepts.net>
2020-04-23 11:17:14 -0400

    Add assertArrayStructure

--- src/Traits/AdditionalAssertions.php
+++ src/Traits/AdditionalAssertions.php
@@ -108,6 +108,44 @@ trait AdditionalAssertions
         }
     }
 
+    public static function assertArrayStructure(array $structure, array $actual)
+    {
+        foreach ($structure as $key => $type) {
+            if (is_array($type) && $key === '*') {
+                PHPUnitAssert::assertIsArray($actual);
+
+                foreach ($actual as $data) {
+                    static::assertArrayStructure($structure['*'], $data);
+                }
+            } elseif (is_array($type) && array_key_exists($key, $structure)) {
+                if (is_array($structure[$key])) {
+                    static::assertArrayStructure($structure[$key], $actual[$key]);
+                }
+            } else {
+                switch ($type) {
+                    case 'string':
+                        PHPUnitAssert::assertIsString($actual[$key]);
+                        break;
+                    case 'integer':
+                        PHPUnitAssert::assertIsInt($actual[$key]);
+                        break;
+                    case 'number':
+                        PHPUnitAssert::assertIsNumeric($actual[$key]);
+                        break;
+                    case 'boolean':
+                        PHPUnitAssert::assertIsBool($actual[$key]);
+                        break;
+                    case 'array':
+                        PHPUnitAssert::assertIsArray($actual[$key]);
+                        break;
+                    default:
+                        PHPUnitAssert::fail('unexpected type: ' . $type);
+                }
+            }
+        }
+    }
+
+
     private function normalizeRules(array $rules)
     {
         return array_map([$this, 'expandRules'], $rules);
